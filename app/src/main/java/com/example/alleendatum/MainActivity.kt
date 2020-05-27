800c1e41ef95375f6dc13a674b157e3b3558189d
peterthys <63521795+peterthys@users.noreply.github.com>
2020-05-27 23:07:48 +0200

    opgekuist staat netjes

--- app/src/main/java/com/example/alleendatum/MainActivity.kt
+++ app/src/main/java/com/example/alleendatum/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.NumberPicker
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
@@ -17,8 +16,8 @@ class MainActivity : AppCompatActivity() {
     var eindTijdInMinuten: Int = 0
     var pauzeUren: Int = 0
     var pauzeMinuten = 30
-    val customerList = arrayOf("customer1", "customer2", "customer3")
-    var aantalMinuten: Int = 0
+    val customerList = arrayOf("Jansen", "Peeters", "Goddefroi")
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,7 +27,6 @@ class MainActivity : AppCompatActivity() {
         pickerUren.minValue = 0
         pickerUren.maxValue = 12
         pickerUren.wrapSelectorWheel = true
-        pickerUren.setOnValueChangedListener { pickerUren, oldVal, newVal -> tv_pauze_uur.setText("$newVal") }
 
 
         var pickerCustomers = findViewById<NumberPicker>(R.id.numberPickerCustomers)
@@ -37,11 +35,6 @@ class MainActivity : AppCompatActivity() {
             pickerCustomers.wrapSelectorWheel = true
             pickerCustomers.maxValue = customerList.size - 1
             pickerCustomers.displayedValues = customerList
-//            pickerCustomers.setOnValueChangedListener { pickerCustomers, oldVal, newVal ->
-//                tv_customer.setText(
-//                    "customer : ($newVal+1)"
-//                )
-//            }
 
         }
         val pickerMinuten = findViewById<NumberPicker>(R.id.numberPickerMinuten)
@@ -49,16 +42,6 @@ class MainActivity : AppCompatActivity() {
         pickerMinuten.maxValue = 60
         pickerMinuten.value = 30
         pickerMinuten.wrapSelectorWheel = true
-        pickerMinuten.setOnValueChangedListener { pickerMinuten, oldVal, newVal ->
-            tv_pauze_minuten.setText(
-                "$newVal"
-            )
-        }
-
-
-        //  pauzeMinuten = pickerMinuten.value
-
-        tv_pauze_minuten.text = pauzeMinuten.toString()
 
 
     }
@@ -105,10 +88,9 @@ class MainActivity : AppCompatActivity() {
     fun berekeningWerkTijd(view: View) {
 
 
-        val edittxt = findViewById<TextView>(R.id.tv_pauze_uur)
-        pauzeUren = edittxt.text.toString().toInt()
-        val edittxt2 = findViewById<TextView>(R.id.tv_pauze_minuten)
-        pauzeMinuten = edittxt2.text.toString().toInt()
+        pauzeUren = numerberPickerUren.value
+
+        pauzeMinuten = numberPickerMinuten.value
         val pauzeTotaal = pauzeUren * 60 + pauzeMinuten
         var werkTijd =
             ((eindTijdInUren * 60) + (eindTijdInMinuten)) - ((beginTijdInUren * 60) + (beginTijdInMinuten)) - pauzeTotaal
@@ -116,18 +98,7 @@ class MainActivity : AppCompatActivity() {
         var werktijdInMinuten: Int = werkTijd - werkTijdInUren * 60
         var resultaat: String =
             werkTijdInUren.toString() + " u  " + werktijdInMinuten.toString() + " min."
-      //  bt_resultaat.text = resultaat
-
-        //    bt_resultaat.text = aantalMinuten.toString()
-
-        //    var som = aantalMinuten + 1
-        //   tv_pauze_uur.text = som.toString()
-        var edict = findViewById<TextView>(R.id.tv_pauze_minuten)
-        aantalMinuten = edict.text.toString().toInt()
-        aantalMinuten= numberPickerMinuten.value
-
-        tv_pauze_uur.text = pauzeMinuten.toString()
-        bt_resultaat.text = aantalMinuten.toString()
+        bt_resultaat.text = resultaat
 
 
     }
