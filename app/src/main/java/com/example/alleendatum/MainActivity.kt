08fce5d265fec63acb547dab2c728eb8faebe66a
peterthys <63521795+peterthys@users.noreply.github.com>
2020-05-27 22:25:58 +0200

    werkt maar nog niet opgeruimd

--- app/src/main/java/com/example/alleendatum/MainActivity.kt
+++ app/src/main/java/com/example/alleendatum/MainActivity.kt
@@ -3,8 +3,8 @@ package com.example.alleendatum
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
 import android.widget.NumberPicker
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     var pauzeUren: Int = 0
     var pauzeMinuten = 30
     val customerList = arrayOf("customer1", "customer2", "customer3")
+    var aantalMinuten: Int = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,6 +30,20 @@ class MainActivity : AppCompatActivity() {
         pickerUren.wrapSelectorWheel = true
         pickerUren.setOnValueChangedListener { pickerUren, oldVal, newVal -> tv_pauze_uur.setText("$newVal") }
 
+
+        var pickerCustomers = findViewById<NumberPicker>(R.id.numberPickerCustomers)
+        if (pickerCustomers != null) {
+            pickerCustomers.minValue = 0
+            pickerCustomers.wrapSelectorWheel = true
+            pickerCustomers.maxValue = customerList.size - 1
+            pickerCustomers.displayedValues = customerList
+//            pickerCustomers.setOnValueChangedListener { pickerCustomers, oldVal, newVal ->
+//                tv_customer.setText(
+//                    "customer : ($newVal+1)"
+//                )
+//            }
+
+        }
         val pickerMinuten = findViewById<NumberPicker>(R.id.numberPickerMinuten)
         pickerMinuten.minValue = 0
         pickerMinuten.maxValue = 60
@@ -39,20 +54,12 @@ class MainActivity : AppCompatActivity() {
                 "$newVal"
             )
         }
-//
-        var pickerCustomers = findViewById<NumberPicker>(R.id.numberPickerCustomers)
-        if (pickerCustomers != null) {
-            pickerCustomers.minValue = 0
-            pickerCustomers.wrapSelectorWheel = true
-            pickerCustomers.maxValue = customerList.size - 1
-            pickerCustomers.displayedValues = customerList
-            pickerCustomers.setOnValueChangedListener { pickerCustomers, oldVal, newVal ->
-                tv_customer.setText(
-                    "customer : $newVal"
-                )
-            }
 
-        }
+
+        //  pauzeMinuten = pickerMinuten.value
+
+        tv_pauze_minuten.text = pauzeMinuten.toString()
+
 
     }
 
@@ -77,6 +84,7 @@ class MainActivity : AppCompatActivity() {
 
     fun eindUurWeergeven(view: View) {
 
+
         val buSelected = view as Button
         when (buSelected.id) {
             bt_eindUur.id -> {
@@ -95,9 +103,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun berekeningWerkTijd(view: View) {
-        val edittxt = findViewById<EditText>(R.id.tv_pauze_uur)
+
+
+        val edittxt = findViewById<TextView>(R.id.tv_pauze_uur)
         pauzeUren = edittxt.text.toString().toInt()
-        val edittxt2 = findViewById<EditText>(R.id.tv_pauze_minuten)
+        val edittxt2 = findViewById<TextView>(R.id.tv_pauze_minuten)
         pauzeMinuten = edittxt2.text.toString().toInt()
         val pauzeTotaal = pauzeUren * 60 + pauzeMinuten
         var werkTijd =
@@ -106,7 +116,19 @@ class MainActivity : AppCompatActivity() {
         var werktijdInMinuten: Int = werkTijd - werkTijdInUren * 60
         var resultaat: String =
             werkTijdInUren.toString() + " u  " + werktijdInMinuten.toString() + " min."
-        bt_resultaat.text = resultaat
+      //  bt_resultaat.text = resultaat
+
+        //    bt_resultaat.text = aantalMinuten.toString()
+
+        //    var som = aantalMinuten + 1
+        //   tv_pauze_uur.text = som.toString()
+        var edict = findViewById<TextView>(R.id.tv_pauze_minuten)
+        aantalMinuten = edict.text.toString().toInt()
+        aantalMinuten= numberPickerMinuten.value
+
+        tv_pauze_uur.text = pauzeMinuten.toString()
+        bt_resultaat.text = aantalMinuten.toString()
+
 
     }
 
